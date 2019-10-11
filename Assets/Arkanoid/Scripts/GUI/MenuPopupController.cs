9528e20f748e5f7738fc17078c13b73168e75c29
Alex03 <yurikov.alex03@gmail.com>
2019-10-11 22:20:43 +0300

    update

--- Assets/Arkanoid/Scripts/GUI/MenuPopupController.cs
+++ Assets/Arkanoid/Scripts/GUI/MenuPopupController.cs
@@ -9,7 +9,7 @@ namespace Arkanoid.GUI
     public class MenuPopupController : MonoBehaviour
     {
         [SerializeField] private Button ContinueBtn, RestartBtn, QuitBtn, MenuBtn;
-        [SerializeField] private AnimationCurve _animationScaleIn,_animationScaleOut, _animationMoveTo;
+        [SerializeField] private AnimationCurve _animationCurve;
         [SerializeField] private Image Vignette;
 
         private Vector3 _tempPosition;
@@ -31,16 +31,14 @@ namespace Arkanoid.GUI
         public void ShowPopupMenu()
         {
             _tempPosition = transform.localPosition;
-            transform.DOScale(Vector3.one, 0.5f).SetEase(_animationMoveTo);
-            //transform.DOLocalJump(Vector3.one, 2f, 1, 0.5f, true);
-            transform.DOLocalMove(Vector3.one, 0.5f, false).SetEase(_animationMoveTo);
+            transform.DOScale(Vector3.one, 0.5f).SetEase(_animationCurve);
+            transform.DOLocalMove(Vector3.one, 0.5f, false).SetEase(_animationCurve);
         }
 
         public void HidePopupMenu()
         {
-            // transform.DOLocalJump(_tempPosition, 2f, 1, 0.5f, true)
-            transform.DOScale(Vector3.zero, 0.5f).SetEase(_animationMoveTo);
-            transform.DOLocalMove(_tempPosition, 0.5f, false).SetEase(_animationMoveTo).OnComplete(() =>
+            transform.DOScale(Vector3.zero, 0.5f).SetEase(_animationCurve);
+            transform.DOLocalMove(_tempPosition, 0.5f, false).SetEase(_animationCurve).OnComplete(() =>
             {
                 Vignette.gameObject.SetActive(false);
                 Time.timeScale = 1;
